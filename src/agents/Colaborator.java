7880910c425b8403945c9cfd7d4bd60caf145266
PedroRomanoBarbosa <pdrobarbosa@gmail.com>
2016-11-06 17:13:04 +0000

    Added more fields in the Colaborator class

--- src/agents/Colaborator.java
+++ src/agents/Colaborator.java
@@ -1,13 +1,17 @@
 package agents;
 
 import java.util.HashMap;
+import java.util.List;
 
 import jade.core.Agent;
+import logic.Task;
 
 public class Colaborator extends Agent{
 	private static final long serialVersionUID = 1L;
 	
 	private HashMap<String,Integer> skills;
+	private List<Task> taskQueue;
+	private String currentTask;
 	private boolean working;
 	
 }
