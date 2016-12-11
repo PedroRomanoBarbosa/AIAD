0fe8996c915643734de7405626a7ef03ca61d885
PedroRomanoBarbosa <pdrobarbosa@gmail.com>
2016-12-11 23:43:35 +0000

    Fixed MetaData bug

--- src/data/MetaData.java
+++ src/data/MetaData.java
@@ -7,5 +7,5 @@ public class MetaData {
 	public final static long projectSetupTime = 5000l;
 	public final static int numberOfProjects = 2;
 	public final static String FIRE = "FIRE";
-	public final static String SINALPHA = "FIRE";
+	public final static String SINALPHA = "SINALPHA";
 }
